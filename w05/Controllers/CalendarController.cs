4dab26446dd7a6af816a9a90ddea6417b7d0c243
JungChi Lin <111B13929@mailst.cjcu.edu.tw>
2024-10-16 11:00:48 +0800

    新增資料庫連接字串至appsettings.json，

---
+++ w05/Controllers/CalendarController.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using w05.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+namespace w05.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+        private readonly string _connectString = DBUtil.ConnectionString();
+
+        [HttpGet]
+        public async Task<IEnumerable<Calendar>> GetCalendars()
+        {
+            string sqlQuery = "SELECT * FROM MyCalendar";
+            using (var connection = new SqlConnection(_connectString))
+            {
+                var Calendars = await connection.QueryAsync<Calendar>(sqlQuery);
+                return Calendars.ToList();
+            }
+        }
+    }
+}
+
+
